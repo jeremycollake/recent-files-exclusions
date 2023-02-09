df6c2f1a9cc3fbfe0db1a3c37dd3227423f41ed1
jeremycollake <jeremy@bitsum.com>
2023-02-09 09:44:49 -0500

    Add versioned links to upload scripts

--- upload-installer-final.cmd
+++ upload-installer-final.cmd
@@ -1,1 +1,10 @@
+@echo off
+IF NOT [%1]==[] GOTO HAVE_ARG
+echo ERROR: Must supply version as arg1 (e.g. 1.1.1.1)
+exit /b 0
+:HAVE_ARG
+echo Uploading static link ...
 scp .\installer\x64\release\RecentFilesExclusionsSetup.exe jeremy@az.bitsum.com:/var/www/vhosts/bitsum.com/files/RecentFilesExclusionsSetup.exe
+echo Uploading versioned link ...
+ssh jeremy@az.bitsum.com "mkdir -p /var/www/vhosts/bitsum.com/files/recentfilesexclusions/%1"
+scp .\installer\x64\release\RecentFilesExclusionsSetup.exe jeremy@az.bitsum.com:/var/www/vhosts/bitsum.com/files/recentfilesexclusions/%1/RecentFilesExclusionsSetup.exe
